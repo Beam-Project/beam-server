3af37177e2939df4204623af52be383c1a91060e
René Bernhardsgrütter <rene.bernhardsgruetter@posteo.ch>
2014-04-24 07:22:01 +0200

    Added the authentication page. But it's still empty.

---
+++ src/main/java/org/beamproject/server/pages/AuthenticationPage.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2013, 2014 inchat.org
+ *
+ * This file is part of inchat-server.
+ *
+ * inchat-server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * inchat-server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.beamproject.server.pages;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * This servlet allows to establish authentication between a client and this
+ * server.
+ */
+@WebServlet(urlPatterns = {"/authentication"})
+public class AuthenticationPage extends Page {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void processMessage() {
+        
+    }
+
+}
---
+++ src/test/java/org/beamproject/server/pages/AuthenticationPageTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2013, 2014 inchat.org
+ *
+ * This file is part of inchat-server.
+ *
+ * inchat-server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * inchat-server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.beamproject.server.pages;
+
+import java.io.IOException;
+import static org.beamproject.common.MessageField.CNT_MSG;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.xml.sax.SAXException;
+
+public class AuthenticationPageTest extends PageTest {
+
+    private AuthenticationPage page;
+
+    @Before
+    public void setDeliveryPageUp() {
+        page = new AuthenticationPage();
+        basicSetup(page);
+    }
+
+    @Test
+    public void testSendingMessage() throws IOException, SAXException {
+        message.putContent(CNT_MSG, "hello".getBytes());
+        setMessageToRequest();
+        response = client.getResponse(request);
+        System.out.println(response.getText());
+    }
+
+}
