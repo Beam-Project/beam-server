3719acb72d76fb0bbb4bac6402f9111578119b1d
René Bernhardsgrütter <rene.bernhardsgruetter@posteo.ch>
2014-08-22 22:49:24 +0200

    Adapted to new concept of MessageHandlers.

--- src/main/java/org/beamproject/server/model/ClientCarrierModelImpl.java
+++ src/main/java/org/beamproject/server/model/ClientCarrierModelImpl.java
@@ -20,10 +20,21 @@ package org.beamproject.server.model;
 
 import org.beamproject.common.carrier.ClientCarrierModel;
 import com.google.inject.Inject;
+import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.WARNING;
+import java.util.logging.Logger;
 import lombok.experimental.Delegate;
+import org.beamproject.common.Message;
 import org.beamproject.common.util.Executor;
 import org.beamproject.common.util.Task;
 import org.beamproject.common.carrier.ClientCarrier;
+import org.beamproject.common.crypto.CryptoPacker;
+import org.beamproject.common.crypto.CryptoPackerPool;
+import org.beamproject.common.crypto.HandshakeResponder;
+import org.beamproject.server.carrier.HandshakeChallengeHandler;
+import org.beamproject.server.carrier.HandshakeSuccessHandler;
+import org.beamproject.server.util.HandshakeStorage;
+import org.beamproject.server.util.SessionStorage;
 
 /**
  * This class implements the {@link ClientCarrierModel} interface and therefore
@@ -31,24 +42,89 @@ import org.beamproject.common.carrier.ClientCarrier;
  */
 public class ClientCarrierModelImpl implements ClientCarrierModel {
 
+    @Inject
+    Logger log;
     private final MainModel model;
     private final Executor executor;
+    private final CryptoPackerPool packerPool;
     @Delegate
     private final ClientCarrier carrier;
+    private final HandshakeStorage<HandshakeResponder> handshakeStorage;
+    private final SessionStorage sessionStorage;
 
     @Inject
-    public ClientCarrierModelImpl(MainModel model, Executor executor, ClientCarrier carrier) {
+    public ClientCarrierModelImpl(MainModel model, Executor executor, ClientCarrier carrier,
+            CryptoPackerPool packerPool, HandshakeStorage<HandshakeResponder> handshakeStorage,
+            SessionStorage sessionStorage) {
         this.model = model;
         this.executor = executor;
         this.carrier = carrier;
+        this.packerPool = packerPool;
+        this.handshakeStorage = handshakeStorage;
+        this.sessionStorage = sessionStorage;
     }
 
     @Override
-    public void consumeMessage(final byte[] message) {
+    public void consumeMessage(final byte[] ciphertext) {
         executor.runAsync(new Task() {
             @Override
             public void run() {
-                System.out.println("consumed message in client carrier: " + new String(message));
+                CryptoPacker packer = null;
+                    log.log(INFO, "an infor message");
+
+                try {
+                    packer = packerPool.borrowObject();
+                    Message message = packer.decryptAndUnpack(ciphertext, model.getServer());
+                    routeMessage(message);
+                } catch (Exception ex) {
+                    log.log(WARNING, "Could not handle an incoming message: {0}", ex.getMessage());
+                } finally {
+                    packerPool.returnObject(packer);
+                }
+            }
+        });
+    }
+
+    private void routeMessage(Message message) {
+        Message response = null;
+
+        switch (message.getType()) {
+
+            case HS_CHALLENGE:
+                HandshakeChallengeHandler challengeHandler = new HandshakeChallengeHandler(handshakeStorage);
+                response = challengeHandler.handle(message);
+                break;
+
+            case HS_SUCCESS:
+                HandshakeSuccessHandler successHandler = new HandshakeSuccessHandler(handshakeStorage, sessionStorage);
+                response = successHandler.handle(message);
+                break;
+
+            default:
+                log.log(INFO, "Received a message of unknown type - ignore it.");
+        }
+
+        if (response != null) {
+            encryptAndSend(response);
+        }
+    }
+
+    @Override
+    public void encryptAndSend(final Message message) {
+        executor.runAsync(new Task() {
+            @Override
+            public void run() {
+                CryptoPacker packer = null;
+
+                try {
+                    packer = packerPool.borrowObject();
+                    byte[] ciphertext = packer.packAndEncrypt(message);
+                    carrier.deliverMessage(ciphertext, message.getRecipient());
+                } catch (Exception ex) {
+                    log.log(WARNING, "Could not send a message: {0}", ex.getMessage());
+                } finally {
+                    packerPool.returnObject(packer);
+                }
             }
         });
     }
---
+++ src/test/java/org/beamproject/server/model/ClientCarrierModelImplTest.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2013, 2014 beamproject.org
+ *
+ * This file is part of beam-server.
+ *
+ * beam-server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * beam-server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.beamproject.server.model;
+
+import java.util.logging.Logger;
+import static java.util.logging.Logger.getLogger;
+import org.beamproject.common.Message;
+import static org.beamproject.common.MessageField.ContentField.*;
+import static org.beamproject.common.MessageField.ContentField.TypeValue.*;
+import org.beamproject.common.Participant;
+import org.beamproject.common.Server;
+import org.beamproject.common.User;
+import org.beamproject.common.carrier.ClientCarrier;
+import org.beamproject.common.crypto.CryptoPacker;
+import org.beamproject.common.crypto.CryptoPackerPool;
+import org.beamproject.common.crypto.CryptoPackerPoolFactory;
+import org.beamproject.common.crypto.EccKeyPairGenerator;
+import org.beamproject.common.crypto.HandshakeResponder;
+import org.beamproject.server.ExecutorFake;
+import org.beamproject.server.util.HandshakeStorage;
+import org.beamproject.server.util.SessionStorage;
+import static org.easymock.EasyMock.*;
+import org.easymock.IAnswer;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+
+public class ClientCarrierModelImplTest {
+
+    private final CryptoPacker PACKER = new CryptoPacker();
+    private final User USER = User.generate();
+    private final Participant USER_WITH_ONLY_PUBLIC_KEY = new Participant(EccKeyPairGenerator.fromPublicKey(USER.getPublicKeyAsBytes()));
+    private final Server SERVER = Server.generate();
+    private MainModel mainModel;
+    private ClientCarrier carrier;
+    private HandshakeStorage<HandshakeResponder> handshakeStorage;
+    private SessionStorage sessionStorage;
+    private ClientCarrierModelImpl model;
+    private Message message;
+
+    @Before
+    public void setUp() {
+        mainModel = createMock(MainModel.class);
+        carrier = createMock(ClientCarrier.class);
+        handshakeStorage = new HandshakeStorage<>();
+        sessionStorage = new SessionStorage();
+
+        model = new ClientCarrierModelImpl(mainModel,
+                new ExecutorFake(),
+                carrier,
+                getPackerPool(),
+                handshakeStorage,
+                sessionStorage);
+        model.log = Logger.getGlobal();
+    }
+
+    private CryptoPackerPool getPackerPool() {
+        CryptoPackerPoolFactory factory = new CryptoPackerPoolFactory();
+        return new CryptoPackerPool(factory);
+    }
+
+    @Test
+    public void testConsumeMessageOnUnknwonType() {
+        message = new Message();
+        message.setRecipient(USER_WITH_ONLY_PUBLIC_KEY);
+        expect(mainModel.getServer()).andReturn(SERVER);
+        replay(mainModel, carrier);
+
+        model.consumeMessage(encrypt(message));
+
+        // nothing should happen
+        verify(mainModel, carrier);
+    }
+
+    @Test
+    public void testConsumeMessageOnWrongRecipient() {
+        message = new Message();
+        message.setRecipient(USER_WITH_ONLY_PUBLIC_KEY);
+        expect(mainModel.getServer()).andReturn(SERVER);
+        replay(mainModel, carrier);
+
+        model.consumeMessage(encrypt(message));
+
+        // nothing should happen
+        verify(mainModel, carrier);
+    }
+
+    @Test
+    public void testEncryptAndSend() {
+        final Message message = new Message(FORWARD, USER);
+        message.putContent(MSG, "my message");
+
+        carrier.deliverMessage(anyObject(byte[].class), anyObject(Participant.class));
+        expectLastCall().andAnswer(new IAnswer<Object>() {
+            @Override
+            public Object answer() {
+                byte[] ciphertext = (byte[]) getCurrentArguments()[0];
+                Participant recipient = (Participant) getCurrentArguments()[1];
+                Message plaintext = PACKER.decryptAndUnpack(ciphertext, recipient);
+
+                assertEquals(USER, message.getRecipient());
+                assertSame(USER, recipient);
+                assertArrayEquals("my message".getBytes(), plaintext.getContent(MSG));
+                return null;
+            }
+        });
+        replay(carrier);
+
+        model.encryptAndSend(message);
+
+        verify(carrier);
+    }
+
+    private byte[] encrypt(Message message) {
+        CryptoPacker packer = new CryptoPacker();
+        return packer.packAndEncrypt(message);
+    }
+
+}
